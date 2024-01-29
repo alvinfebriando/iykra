421a7d55d1413ec0e480a370b5d238cc22f368ad
Alvin Febriando <alvin.febriando@shopee.com>
2024-01-29 23:20:22 +0700

    feat: add email and password field to user entity

--- entity/user.go
+++ entity/user.go
@@ -4,6 +4,8 @@ import "time"
 
 type User struct {
 	Id          uint      `gorm:"primaryKey;autoIncrement"`
+	Email       string    `gorm:"not null;unique"`
+	Password    string    `gorm:"not null"`
 	Name        string    `gorm:"not null"`
 	Address     string    `gorm:"not null"`
 	DateOfBirth time.Time `gorm:"not null"`
