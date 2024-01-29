5558c955907e2cd7157360f1ddaa135e248658e0
Alvin Febriando <alvin.febriando@shopee.com>
2024-01-29 22:21:18 +0700

    feat: register customer handler

--- cmd/rest/rest.go
+++ cmd/rest/rest.go
@@ -1,12 +1,28 @@
 package main
 
 import (
+	"log"
+
+	"github.com/alvinfebriando/costumer-test/handler"
+	"github.com/alvinfebriando/costumer-test/repository"
 	"github.com/alvinfebriando/costumer-test/router"
 	"github.com/alvinfebriando/costumer-test/server"
+	"github.com/alvinfebriando/costumer-test/usecase"
 )
 
 func main() {
-	handlers := router.Handlers{}
+	db, err := repository.GetConnection()
+	if err != nil {
+		log.Printf("failed to get db connection: %s\n", err)
+	}
+
+	userRepository := repository.NewUserRepository(db)
+	customerUsecase := usecase.NewCustomerUsecase(userRepository)
+	customerHandler := handler.NewCustomerHandler(customerUsecase)
+
+	handlers := router.Handlers{
+		Customer: customerHandler,
+	}
 
 	r := router.New(handlers)
 	s := server.New(r)
--- router/router.go
+++ router/router.go
@@ -3,15 +3,20 @@ package router
 import (
 	"net/http"
 
+	"github.com/alvinfebriando/costumer-test/handler"
 	"github.com/gin-gonic/gin"
 )
 
-type Handlers struct{}
+type Handlers struct {
+	Customer *handler.CustomerHandler
+}
 
-func New(handlers Handlers) http.Handler {
+func New(h Handlers) http.Handler {
 	r := gin.New()
 
+	r.GET("/customers", h.Customer.ListCustomer)
+
 	r.Use(gin.Recovery())
-	
+
 	return r
 }
